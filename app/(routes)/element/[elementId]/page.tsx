21d16b7df3886103e2bb97c418704ff42ed47a38
KevinStevenGG00 <150202462+KevinStevenGG00@users.noreply.github.com>
2024-12-02 00:57:34 -0500

    fix: fix params in elementId page 2

--- app/(routes)/element/[elementId]/page.tsx
+++ app/(routes)/element/[elementId]/page.tsx
@@ -6,13 +6,13 @@ import { redirect } from "next/navigation";
 export default async function ElementPage({
   params,
 }: {
-  params: { elementId: string };
+  params: Promise<{ elementId: string }>;
 }) {
-  const { elementId } = params; // No es necesario Promise.resolve()
+  const { elementId } = await params; // Esperamos la resolución de params
 
   const session = await getServerSession();
   if (!session || !session.user?.email) {
-    return redirect("/");
+    redirect("/");
   }
 
   const element = await db.element.findUnique({
@@ -22,7 +22,7 @@ export default async function ElementPage({
   });
 
   if (!element) {
-    return redirect("/"); // Asegúrate de retornar aquí
+    redirect("/");
   }
 
   return (
