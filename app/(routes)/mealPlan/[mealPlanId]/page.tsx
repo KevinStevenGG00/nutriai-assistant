8de46c1a3b15faf7fe1e425d7cdc062548c25fc9
KevinStevenGG00 <150202462+KevinStevenGG00@users.noreply.github.com>
2024-12-02 01:02:47 -0500

    fix: fix params in mealPlanId page

--- app/(routes)/mealPlan/[mealPlanId]/page.tsx
+++ app/(routes)/mealPlan/[mealPlanId]/page.tsx
@@ -6,9 +6,9 @@ import { redirect } from "next/navigation";
 export default async function MealPlanPage({
   params,
 }: {
-  params: { mealPlanId: string };
+  params: Promise<{ mealPlanId: string }>;
 }) {
-  const { mealPlanId } = await Promise.resolve(params);
+  const { mealPlanId } = await params;
 
   const session = await getServerSession();
   if (!session || !session.user?.email) {
